c53b60b8e875181776da4122b7bafc866fe2ec00
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-09-05 14:47:08 -0400

    router created main.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-server
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"go-server/controller"
+	"go-server/model"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/register", controller.RegisterHandler).
+		Methods("POST")
+	r.HandleFunc("/login", controller.LoginHandler).
+		Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
